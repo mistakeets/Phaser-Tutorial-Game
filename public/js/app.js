1d150f187bfcf7f3bc5dbd65f4189facb4bf70b1
Keith Oppel <mistakeets@gmail.com>
2017-05-05 16:10:22 -0700

    finishes update of game loading function

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,21 @@
-var canvas = document.createElement('canvas')
-var ctx = canvas.getContext('2d')
-canvas.width = 512
-canvas.height = 480
-document.appendChild(canvas)
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
+  preload: preload,
+  create: create,
+  update,
+  update
+})
 
-var lastTime
+function preload() {
+  game.load.image('sky', 'assets/sky.png');
+  game.load.image('ground', 'assets/platform.png');
+  game.load.image('star', 'assets/star.png');
+  game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+}
 
-function main() {
-  var now = Date.now()
-  var dt = (now - lastTime) / 1000.0
+function create() {
+  game.add.sprite(0, 0, 'star')
+}
 
-  update(dt)
-  render()
+function update() {
 
-  lastTime = now
-  requestAnimationFrame(main)
 }
