95fa6ded92c046145d66380776780de2a3e258f7
Keith Oppel <mistakeets@gmail.com>
2017-05-07 20:13:26 -0700

    fixes update function to resolve movement issues and collision checking

--- public/js/app.js
+++ public/js/app.js
@@ -62,10 +62,16 @@ function create() {
 }
 
 function update() {
-  var hitPlatform = game.physics.arcade.collide(player, platforms)
+  game.physics.arcade.collide(player, platforms)
+  game.physics.arcade.collide(stars, platforms)
+
+  game.physics.arcade.overlap(player, stars, collectStar, null, this)
+
+  player.body.velocity.x = 0
 
   if (cursors.left.isDown) {
-    player.body.velocity.x = 150
+    player.body.velocity.x = -150
+    player.animations.play('left')
   } else if (cursors.right.isDown) {
     player.body.velocity.x = 150
     player.animations.play('right')
@@ -73,7 +79,7 @@ function update() {
     player.animations.stop()
     player.frame = 4
   }
-  if (cursors.up.isDown && player.body.touch.down && hitPlatform) {
+  if (cursors.up.isDown && player.body.touching.down) {
     player.body.velocity.y = -350
   }
 }
